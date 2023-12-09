6dc30f3843c948f6faf3b07205632a8cd37d47d6
LinhTran <trannhatlinh120801@gmail.com>
2023-12-09 16:53:50 +0700

    first commit

---
+++ app/src/main/java/wade/owen/watt/note_app/ui/theme/app_color/Palette.kt
@@ -0,0 +1,11 @@
+package wade.owen.watt.note_app.ui.theme.app_color
+
+import androidx.compose.ui.graphics.Color
+
+sealed class Palette {
+    val Lavender = Color(0xFFFD99FF)
+    val SalmonPink = Color(0xFFFF9E9E)
+    val LightGreen = Color(0xFF91F48F)
+    val PastelYellow = Color(0xFF99A3FF)
+    val Waterspout = Color(0xFFFF9E9E)
+}
